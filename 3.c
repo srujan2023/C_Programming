0e57da8fb7831ede31ab904373ff6fbb62243e6f
Srujan <Srujan2004ab@gmail.com>
2025-08-22 21:54:58 +0530

    Most Used Variables

---
+++ 3.c
@@ -0,0 +1,23 @@
+//Most Used Variables
+
+#include<stdio.h>
+
+int main(){
+    
+    //Int Variable is stored only Numbers
+    int number = 100;
+
+    //Float Variable is stored only decimal Values Then After Point Stored in only 6 digits
+    float decimal = 99.990979;
+
+    //Double Variable is stored only decimal Values Then After Point Stored in More Than 6 digits
+    double decmial_2 = 89.07585723;
+
+    //Char is a take Any Values Only For Between Strings
+    char Name = 'A';
+
+    //Value Print Code
+    printf("%d \n%f\n%f\n%2lc",number,decimal,decmial_2,Name);
+
+    return 0;
+}
