76c06ee29fe132ad5069dff5d33721f44dcaa961
Srujan <Srujan2004ab@gmail.com>
2025-08-23 20:51:30 +0530

    Add two numbers using arithmetic opertions

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+   int num1,num2;
+    printf("Enter your two numbers \n");
+    printf("Enter your first number \n");
+    scanf("%d",&num1);
+    printf("Enter your second number \n");
+     scanf("%d",&num2);
+    printf("%d",num1+num2);
+    return 0;
+}
