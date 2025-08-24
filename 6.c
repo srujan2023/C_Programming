1b0a301cbaf48970f6ec1a5179d37f1f00f7057d
Srujan <Srujan2004ab@gmail.com>
2025-08-24 19:48:01 +0530

    All arithmetic  using program

---
+++ 6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int num1,num2;
+    printf("Enter Your two numbers\n");
+    printf("Enter your first numbers\n");
+    scanf("%d",&num1);
+    printf("Enter your Second numbers\n");
+    scanf("%d",&num2);
+    printf("Addition of two values:%d\n",num1+num2);
+    printf("Subtraction of two values:%d\n",num1-num2);
+    printf("Mutlipication of two values:%d\n",num1*num2);
+    printf("Division of two values:%d\n",num1/num2);
+    printf("Reminder of two values:%d\n",num1%num2);
+    return 0;
+}
