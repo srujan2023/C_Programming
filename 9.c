4b82cfa6c8e447b73a1f9423209202ee55c285e4
Srujan <Srujan2004ab@gmail.com>
2025-08-24 20:34:46 +0530

    if ladders and nested if program

---
+++ 9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a = 30, b =25, c =10;
+    if(a > b){
+
+         if(b > c){
+                 printf("A is Big");
+      }
+         else{
+                printf("A is small");
+    }
+}
+    else{
+        printf("A is Smaller than B");
+    }
+    return 0;
+}
