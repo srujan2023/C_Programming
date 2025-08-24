1a08050305823b983fa1eda1f99e7c9acff8c8ae
Srujan <Srujan2004ab@gmail.com>
2025-08-24 19:35:34 +0530

    Add two Numbers

---
+++ 5o.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int num1,num2;
+    printf("Enter your Two numbers\n");
+    printf("Enter your First numbers\n");
+    scanf("%d",&num1);
+    printf("Enter your Second numbers\n");
+    scanf("%d",&num2);
+    printf("Addition Two numbers:%d",num1 + num2);
+    return 0;
+}
