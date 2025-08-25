6b1edd6eda3af63fde0284ad7451ee7bba3ea01d
Srujan <Srujan2004ab@gmail.com>
2025-08-25 16:31:46 +0530

    Arrays values take from users

--- 12.c
+++ 12.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    int array[10] = {45,67,98,85,06};
-    printf("Values Of ararys::%d\n",array[0]);
+    int array[10];
+    printf("Add Element to the Array:\n");
+    for(int i=0; i < 5; i++ ){
+        scanf("%d",&array[i]);
+    }
+    printf("Values Of ararys::\n");
+    for(int i=0;i<5;i++){
+        printf(" %d ",array[i]);
+    }
     return 0;
 }
