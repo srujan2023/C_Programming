608a90749dd6ec82ae83d82c518e5b4c6df95902
Srujan <Srujan2004ab@gmail.com>
2026-01-09 22:49:34 +0530

    pre and post increment

---
+++ gate4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+  int x = 40;
+
+    int main(){
+        printf("%d\n",30>40);
+        printf("%d\n",22<23);
+         printf("%d\n",30==30);
+         printf("%d\n",34!=24);
+         printf("%d\n",54<=50);
+         printf("%d\n",54>=50);
+    return 0;
+}
