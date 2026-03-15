9b4ea098a987fe4df92be29b956d3684fad4ee7e
srujan <srujan2004ab@gmail.com>
2026-03-15 22:13:37 +0530

    Sample Program

--- Practice/01.c
+++ Practice/01.c
@@ -2,5 +2,9 @@
 int main(){
     int num1 = 10;
     int num2 = 25;
-    printf("%d",num1+num2);
+    printf("%d\n",num1+num2);
+    printf("%d\n",num1-num2);
+    printf("%d\n",num1*num2);
+    printf("%d\n",num1/num2);
+    printf("%d\n",num1%num2);
 }
