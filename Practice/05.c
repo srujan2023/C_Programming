8f023590a8b03f81b1d49aefb6cb06ec67688be9
srujan <srujan2004ab@gmail.com>
2026-03-16 22:57:46 +0530

    Temperature Updated

---
+++ Practice/05.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    float temperature;
+
+    printf("Enter temperature:");
+    scanf("%f",&temperature);
+
+    if(temperature<=20){
+        printf("Wear a sweater!");
+    }
+    
+     else if(temperature>30){
+        printf("Stay Safe!\n");
+    }
+
+    else if(temperature>20 || temperature<30){
+        printf("Normal\n");
+    }
+   
+    else{
+        printf("Wear a Tshirt and drink more water\n");
+    }
+    
+    return 0;
+}
