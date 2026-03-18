f91e7ccb297c56af52cd71583ad080599705b0ea
srujan <srujan2004ab@gmail.com>
2026-03-18 23:17:43 +0530

    Student Marks Average Program

--- Practice/Arrays.c
+++ Practice/Arrays.c
@@ -2,6 +2,8 @@
 
 int main(){
     int Marks[4],i;
+    double Average;
+    int sum=0;
     printf("Enter First Test Marks\n");
 
     for(i=0;i<4;i++){
@@ -11,5 +13,8 @@ int main(){
     printf("This is Your Student Marks\n");
     for(i=0;i<4;i++){
     printf("%d ",Marks[i]);
+    sum=sum+Marks[i];
     }
+    Average=sum/4;
+    printf("\nAverage Marks: %.2f",Average);
 }
