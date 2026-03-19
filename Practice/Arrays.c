e367da53eab11f983b92af5f85594fad9cf40f08
srujan <srujan2004ab@gmail.com>
2026-03-19 23:02:08 +0530

    Updated

--- Practice/Arrays.c
+++ Practice/Arrays.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
 
 int main(){
-    int Marks[4],i;
+    int n=4,i;
+    int marks[n];
     double Average;
     int sum=0;
     printf("Enter First Test Marks\n");
 
-    for(i=0;i<4;i++){
-    scanf("%d",&Marks[i]);
+    for(i=0;i<n;i++){
+    scanf("%d",&marks[i]);
     }
+    marks[0] = 100;
+
+    marks[n-1] = 100;
+
 
     printf("This is Your Student Marks\n");
-    for(i=0;i<4;i++){
-    printf("%d ",Marks[i]);
-    sum=sum+Marks[i];
+    for(i=0;i<n;i++){
+    printf("%d ",marks[i]);
+    sum=sum+marks[i];
     }
     Average=sum/4;
     printf("\nAverage Marks: %.2f",Average);
