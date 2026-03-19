79133593f6398c09ed5956682104628f012b54da
srujan <srujan2004ab@gmail.com>
2026-03-19 22:56:07 +0530

    Matrix Program

---
+++ Practice/Matrix.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+    int matrix[3][3];
+printf("Enter Matrix Elements\n");
+for(int i=0;i<3;i++){
+for(int j=0;j<3;j++){
+scanf("%d",&matrix[i][j]);
+}
+}
+printf("Enter Matrix \n");
+for(int i=0;i<3;i++){
+for(int j=0;j<3;j++){
+    printf("%d ",matrix[i][j]);
+}
+    printf("\n");
+}
+}
+
