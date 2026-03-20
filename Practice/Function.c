0d9e59dd57f726624b0a833bf4117ec00e0ebb46
srujan <srujan2004ab@gmail.com>
2026-03-20 22:44:28 +0530

    Function Program Updated

--- Practice/Function.c
+++ Practice/Function.c
@@ -4,7 +4,18 @@ void greet(){
     printf("Hello, World!\n");
 }
 
+int add(int a, int b){
+    int c;
+    c = a + b;
+    return c;
+}
+
 int main(){
+    int sum, n, m;
 greet();
+printf("Enter two numbers: ");
+scanf("%d %d", &n, &m);
+sum = add(n, m);
+printf("The sum is: %d\n", sum);
 return 0;
 }
